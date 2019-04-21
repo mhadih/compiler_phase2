06310f32622285e6a52a7ff13f466290aadd4b64
mahdi <hadiloomehdi@gmail.com>
2019-04-21 16:30:56 +0430

    MethodSymbolTable

---
+++ src/toorla/symbolTable/symbolTableItem/MethodSymbolTableItem.java
@@ -0,0 +1,32 @@
+package toorla.symbolTable.symbolTableItem;
+
+import toorla.symbolTable.symbolTableItem.varItems.VarSymbolTableItem;
+import toorla.types.Type;
+
+import java.util.ArrayList;
+
+
+public class MethodSymbolTableItem  extends SymbolTableItem {
+    private String name;
+    private Type methodType;
+    private ArrayList <Type> parameters;
+
+    public MethodSymbolTableItem(String name,Type methodType,ArrayList <Type> parameters) {
+        this.name=name;
+        this.methodType=methodType;
+        this.parameters=parameters;
+    }
+
+    public String getKey() {
+        return name;
+    }
+
+    public Type getMethodType() {
+        return methodType;
+    }
+
+    public ArrayList <Type> getParameters() {
+        return parameters;
+    }
+
+}
